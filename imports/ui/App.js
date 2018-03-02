3fe5d9d908b8f14d02ed0b6db8fe32cabae4dbb4
Thiago Marinho <tgmarinho@gmail.com>
2018-03-01 21:52:58 -0400

    #7 GraphQL Queries in React - Full-stack GraphQL with Apollo, Meteor & React

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
-const App = () => (<h1>Hello Meteor com React</h1>);
+const App = ({data}) => (<h1>{data.hi}</h1>);
 
-export default App;
+
+const hiQuery = gql`
+{
+  hi
+}
+`;
+
+
+export default graphql(hiQuery)(App);
